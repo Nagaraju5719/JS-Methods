5adf4d0c87dc5fd4e6b6fee8f1e64793b8067d51
Nagaraju <85275377+Nagaraju5719@users.noreply.github.com>
2022-11-08 11:49:37 +0530

    Update ConsecutiveNum.js

--- InterviewCodingQA/ConsecutiveNum.js
+++ InterviewCodingQA/ConsecutiveNum.js
@@ -1,17 +1,19 @@
 // # FINDING MISSING SEQUENCE NUMBERS IN AN ARRAY //
 
-
+let arr2 = [1, 5, 3, 9, 0, 7, 8];
 let a=[1,2,3,4,5,6,7,8,11]
 
 
 function findMissingNum(a){
-    for (let i =0; i<a.length - 1; ++i){
-        if(a[i] + 1 !== a[i + 1]){
-            return a[i] + 1
-        }
-    }
-    return null
+  let newArr1 = [];
+for (let x=1; x<=a[a.length-1]; x++) {
+  if (!a.includes(x)) {
+    newArr1.push(x);
+  }
+}
+return newArr1
 }
 
 
-console.log(findMissingNum(a)) //9
+console.log(findMissingNum(a)) // [ 9, 10 ]
+console.log(findMissingNum(arr2)) // [ 2, 4, 6 ]
